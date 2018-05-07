8a138bab2b554229e7a911c0d8cde389fc8bd83f
tdimnet <thomas.dimnet@gmail.com>
2018-05-07 11:09:46 +0200

    Add example for protocol and its implentation

--- Protocols.playground/Contents.swift
+++ Protocols.playground/Contents.swift
@@ -1,10 +1,27 @@
+// A protocol only describes an implementation
+protocol FullNameable {
+    var fullName: String { get }
+}
 
 
+struct User: FullNameable {
+    var fullName: String
+}
 
+let user = User(fullName: "John Smith")
 
+struct Friend: FullNameable {
+    let firstName: String
+    let middleName: String
+    let lastName: String
+    
+    var fullName: String {
+        return "\(firstName) \(middleName) \(lastName)"
+    }
+}
 
-
-
+let friend = Friend(firstName: "Thomas", middleName: "Stephane", lastName: "Dimnet")
+friend.fullName
 
 
 
