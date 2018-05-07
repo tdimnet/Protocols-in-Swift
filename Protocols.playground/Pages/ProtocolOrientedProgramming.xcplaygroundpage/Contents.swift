5e51c93ac18655e3e58b4f587e84a780e7bd9d5c
tdimnet <thomas.dimnet@gmail.com>
2018-05-07 16:46:51 +0200

    Start working with protocol oriented programming

---
+++ Protocols.playground/Pages/ProtocolOrientedProgramming.xcplaygroundpage/Contents.swift
@@ -0,0 +1,93 @@
+enum Direction {
+    case up
+    case down
+    case left
+    case right
+}
+
+protocol Movable {
+    func move(_ direction: Direction, by distance: Int)
+}
+
+protocol Destructable {
+    func decreaseLife(by factor: Int)
+}
+
+protocol Attackable {}
+
+struct Point {
+    let x: Int
+    let y: Int
+    
+    init(x: Int, y: Int) {
+        self.x = x
+        self.y = y
+    }
+    
+    /// Returns the surrounding points in range of
+    /// the current one
+    func points(inRange range: Int = 1) -> [Point] {
+        var results = [Point]()
+        
+        let lowerBoundOfXRange = x - range
+        let upperBoundOfXRange = x + range
+        
+        let lowerBoundOfYRange = y - range
+        let upperBoundOfYRange = y + range
+        
+        for xCoordinate in lowerBoundOfXRange...upperBoundOfXRange {
+            for yCoordinate in lowerBoundOfYRange...upperBoundOfYRange {
+                let coordinatePoint = Point(x: xCoordinate, y: yCoordinate)
+                results.append(coordinatePoint)
+            }
+        }
+        
+        return results
+    }
+}
+
+// Enemy
+class Enemy {
+    var life: Int = 2
+    let position: Point
+    
+    required init(x: Int, y: Int) {
+        self.position = Point(x: x, y: y)
+    }
+    
+    func decreaseLife(by factor: Int) {
+        life -= factor
+    }
+}
+
+// Tower
+class Tower {
+    let position: Point
+    var range: Int = 1
+    var strength: Int = 1
+    
+    init(x: Int, y: Int) {
+        self.position = Point(x: x, y: y)
+    }
+    
+    func fire(at enemy: Enemy) {
+        if isInRange(of: enemy) {
+            enemy.decreaseLife(by: strength)
+            print("Gotcha")
+        } else {
+            print("Darn! Out of range!")
+        }
+    }
+    
+    func isInRange(of enemy: Enemy) -> Bool {
+        let availablePositions = position.points(inRange: range)
+        
+        for point in availablePositions {
+            if point.x == enemy.position.x && point.y == enemy.position.y {
+                return true
+            }
+        }
+        
+        return false
+    }
+}
